2b89e16b0239f1c4011b5b0c45f4e72558ae050e
Demonic <134345277+IKomangYoga@users.noreply.github.com>
2024-11-22 14:17:58 +0800

    flowbite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    $res=Http::get(env('APP_API_URL').'/users');
+    $json=$res->json();
+    return view('welcome',['users'=>$json['data']]);
+});
+
+Route::get('/main',function ()  {
+    return view('admin.dashboard');
 });
