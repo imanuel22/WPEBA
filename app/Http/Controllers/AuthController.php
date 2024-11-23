fa49947603243fb149850414aa7449ea47f3ac3a
imanuel22 <imanuelrenra220@gmail.com>
2024-11-23 12:49:57 +0800

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view('auth.login');
+    }
+
+    public function dologin(Request $request){
+        $credentials = $request->only(['email', 'password']);
+
+        $res=Http::post(env('APP_API_URL').'/login',$credentials);
+        if ($res->successful()) {
+            $json = $res->json();
+            $userData=$json['data'];
+       
+            Session::put([
+                'token'=>$json['token'],
+                'name'=>$userData['name'],
+                'email'=>$userData['email'],
+                'profile'=>$userData['profile'],
+                'role'=>$userData['role'],
+            ]);
+
+            if($userData['role']=='admin'){
+                return redirect('/admin/dashboard');
+            }
+            if($userData['role']=='organizer'){
+                return redirect('organizer/dashbord') ;  
+            }
+            if($userData['role']=='partisipan'){
+                return redirect('/partisipan/dashboard');
+            }
+        }else{
+
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::get('/login', function () {
-    $res=Http::get(env('APP_API_URL').'/users');
-    $json=$res->json();
-    return view('welcome',['users'=>$json['data']]);
-});
+// Route::get('/login', function () {
+//     $res=Http::get(env('APP_API_URL').'/users');
+//     $json=$res->json();
+//     return view('welcome',['users'=>$json['data']]);
+// });
 
 Route::get('/main',function ()  {
     return view('admin.dashboard');
 });
+
+Route::get('/login',[AuthController::class,'login']);
+Route::post('/dologin',[AuthController::class,'dologin'])->name('dologin');
