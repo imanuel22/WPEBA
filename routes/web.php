140c3c31a0492e5d4410b790b20b3f62266ec1aa
KadekAgusAdi <134345319+KadekAgusAdi@users.noreply.github.com>
2024-12-20 16:19:45 +0800

    coba di chek ren

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,22 @@ Route::prefix('/admin')->group(function(){
     // Route::get('/',);
     Route::get('/dashboard',[AdminController::class,'dashboard']);
     Route::get('/events',[AdminController::class,'events']);
+    Route::get('/tickets', [AdminController::class, 'manageTickets']);
+    Route::get('/documents', [AdminController::class, 'manageDocuments']);
+    Route::get('/feedbacks', [AdminController::class, 'manageFeedbacks']);
+    Route::get('/organizers', [AdminController::class, 'manageOrganizers']);
+    Route::get('/participants', [AdminController::class, 'manageParticipants']);
+    Route::get('/profile', [AdminController::class, 'manageProfile']);
+    Route::get('/admins', [AdminController::class, 'manageAdmins']);
+    Route::get('/categories', [AdminController::class, 'manageCategories'])->name('admin.categories');
+    Route::post('/categories/add', [AdminController::class, 'addCategory'])->name('admin.addCategory');
+    Route::get('/categories/edit/{id}', [AdminController::class, 'editCategory'])->name('admin.editCategory');
+    Route::delete('/categories/delete/{id}', [AdminController::class, 'deleteCategory'])->name('admin.deleteCategory');
+
+    // Routes untuk registrations
+    Route::get('/registrations', [AdminController::class, 'manageRegistrations'])->name('admin.registrations');
+    Route::get('/registrations/edit/{id}', [AdminController::class, 'editRegistration'])->name('admin.editRegistration');
+    Route::delete('/registrations/delete/{id}', [AdminController::class, 'deleteRegistration'])->name('admin.deleteRegistration');
 });
 
 // organizer
