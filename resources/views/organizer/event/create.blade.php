12953eb893f9df37c07f95bb1a3b5c513e36f655
imanuel22 <imanuelrenra220@gmail.com>
2025-01-07 21:27:45 +0800

    update desain

--- resources/views/organizer/event/create.blade.php
+++ resources/views/organizer/event/create.blade.php
@@ -1,18 +1,21 @@
 @extends('organizer.partials.main')
 
 @section('main')
-    <div class="">
+    <div class="mb-3">
+        <h1 class="text-4xl text-center">Create Event</h1>
+    </div>
+    <div class="p-6 bg-slate-700 rounded-3xl">
         <form action="/organizer/event/create" method="POST" class="" enctype="multipart/form-data">
             @csrf
             <div class="mb-5">
-                <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your title</label>
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-200">Your title</label>
                 <input name="title" type="text" id="title"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                     value="{{ old('title') }}" required />
             </div>
 
             <div class="mb-5">
-                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                <label for="description" class="block mb-2 text-sm font-medium text-gray-200">Your
                     description</label>
                 <textarea name="description" id="description" rows="4"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -20,49 +23,51 @@
             </div>
 
             <div class="mb-5">
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="images">Upload images
+                <label class="block mb-2 text-sm font-medium text-gray-200" for="images">Upload images
                     (Max 5)</label>
                 <input
                     class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                     id="images" type="file" name="images[]" accept="image/jpeg,image/png,image/jpg" multiple>
             </div>
 
+            <div class="grid grid-cols-3 gap-4">
 
+                <div class="mb-5">
+                    <label for="start_datetime" class="block mb-2 text-sm font-medium text-gray-200">Start Date
+                        and Time</label>
+                    <input type="datetime-local" id="start_datetime" name="start_datetime"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        value="{{ old('start_datetime') }}">
+                </div>
 
-            <div class="mb-5">
-                <label for="start_datetime" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date
-                    and Time</label>
-                <input type="datetime-local" id="start_datetime" name="start_datetime"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    value="{{ old('start_datetime') }}">
+                <div class="mb-5">
+                    <label for="duration" class="block mb-2 text-sm font-medium text-gray-200">Duration (in
+                        minutes)</label>
+                    <input type="number" id="duration" name="duration"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        value="{{ old('duration') }}">
+                </div>
+                <div class="mb-5">
+                    <label for="location" class="block mb-2 text-sm font-medium text-gray-200">Location</label>
+                    <input type="text" id="location" name="location"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        value="{{ old('location') }}">
+                </div>
             </div>
 
-            <div class="mb-5">
-                <label for="duration" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Duration (in
-                    minutes)</label>
-                <input type="number" id="duration" name="duration"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    value="{{ old('duration') }}">
-            </div>
 
-            <div class="mb-5">
-                <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
-                <input type="text" id="location" name="location"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    value="{{ old('location') }}">
-            </div>
-            <div class="mb-5">
-                <label for="event_category_ids"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Categories</label>
-                <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-12 md:grid-cols-6">
+            <div class="mb-5 ">
+                <label for="event_category_ids" class="block mb-2 text-sm font-medium text-gray-200">Categories</label>
+                <div
+                    class="grid items-baseline grid-cols-1 p-3 bg-white rounded-lg sm:grid-cols-3 lg:grid-cols-10 md:grid-cols-5">
                     @foreach ($categories as $category)
-                        <div class="flex items-center mb-2">
+                        <div class="flex items-center mx-3 my-1">
                             <input id="category-{{ $category['id'] }}" type="checkbox" name="event_category_ids[]"
                                 value="{{ $category['id'] }}"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                                class="items-baseline w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
                                 {{ old('event_category_ids') ? 'checked' : '' }}>
                             <label for="category-{{ $category['id'] }}"
-                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                class="items-baseline ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                                 {{ $category['name'] }}
                             </label>
                         </div>
--- resources/views/organizer/event/edit.blade.php
+++ resources/views/organizer/event/edit.blade.php
@@ -1,75 +1,81 @@
 @extends('organizer.partials.main')
 
 @section('main')
-    <div class="">
+    <div class="mb-3">
+        <h1 class="text-4xl text-center">Update Event</h1>
+    </div>
+    <div class="p-6 rounded-lg bg-slate-700">
         <form action="" method="POST" class="" enctype="multipart/form-data">
             @csrf
             @method('PUT')
 
             <div class="mb-5">
-                <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your title</label>
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-200">Your title</label>
                 <input name="title" type="text" id="title"
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                     value="{{ old('title', $event['title']) }}" required />
             </div>
 
             <div class="mb-5">
-                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                <label for="description" class="block mb-2 text-sm font-medium text-gray-200">Your
                     description</label>
-                <textarea name="description" id="description" rows="4"
+                <textarea name="description" id="description" rows="10"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Leave a comment...">{{ old('description', $event['description']) }}</textarea>
             </div>
 
             <div class="mb-5">
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="images">Upload images
+                <label class="block mb-2 text-sm font-medium text-gray-200" for="images">Upload images
                     (Max 5)</label>
                 <input
                     class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                     id="images" type="file" name="images[]" accept="image/jpeg,image/png,image/jpg" multiple>
             </div>
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
 
-            <div class="mb-5">
-                <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
-                <select id="status" name="status"
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="" {{ old('status', $event['status']) == '' ? 'selected' : '' }}>Select status
-                    </option>
-                    <option value="upcoming" {{ old('status', $event['status']) == 'upcoming' ? 'selected' : '' }}>Upcoming
-                    </option>
-                    <option value="in_progress" {{ old('status', $event['status']) == 'in_progress' ? 'selected' : '' }}>In
-                        Progress</option>
-                    <option value="completed" {{ old('status', $event['status']) == 'completed' ? 'selected' : '' }}>
-                        Completed</option>
-                </select>
-            </div>
+                <div class="mb-5">
+                    <label for="status" class="block mb-2 text-sm font-medium text-gray-200">Status</label>
+                    <select id="status" name="status"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        </option>
+                        <option value="upcoming" {{ old('status', $event['status']) == 'upcoming' ? 'selected' : '' }}>
+                            Upcoming
+                        </option>
+                        <option value="in_progress"
+                            {{ old('status', $event['status']) == 'in_progress' ? 'selected' : '' }}>In
+                            Progress</option>
+                        <option value="completed" {{ old('status', $event['status']) == 'completed' ? 'selected' : '' }}>
+                            Completed</option>
+                    </select>
+                </div>
 
-            <div class="mb-5">
-                <label for="start_datetime" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date
-                    and Time</label>
-                <input type="datetime-local" id="start_datetime" name="start_datetime"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    value="{{ old('start_datetime', $event['start_datetime'] ? $event['start_datetime'] : '') }}">
-            </div>
+                <div class="mb-5">
+                    <label for="start_datetime" class="block mb-2 text-sm font-medium text-gray-200">Start Date
+                        and Time</label>
+                    <input type="datetime-local" id="start_datetime" name="start_datetime"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        value="{{ old('start_datetime', $event['start_datetime'] ? $event['start_datetime'] : '') }}">
+                </div>
 
-            <div class="mb-5">
-                <label for="duration" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Duration (in
-                    minutes)</label>
-                <input type="number" id="duration" name="duration"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    value="{{ old('duration', $event['duration']) }}">
-            </div>
+                <div class="mb-5">
+                    <label for="duration" class="block mb-2 text-sm font-medium text-gray-200">Duration (in
+                        minutes)</label>
+                    <input type="number" id="duration" name="duration"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        value="{{ old('duration', $event['duration']) }}">
+                </div>
 
-            <div class="mb-5">
-                <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
-                <input type="text" id="location" name="location"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                    value="{{ old('location', $event['location']) }}">
+                <div class="mb-5">
+                    <label for="location" class="block mb-2 text-sm font-medium text-gray-200">Location</label>
+                    <input type="text" id="location" name="location"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        value="{{ old('location', $event['location']) }}">
+                </div>
             </div>
             <div class="mb-5">
-                <label for="event_category_ids"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Categories</label>
-                <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-12 md:grid-cols-6">
+                <label for="event_category_ids" class="block mb-2 text-sm font-medium text-gray-200">Categories</label>
+                <div
+                    class="grid items-baseline grid-cols-1 p-3 bg-white rounded-lg sm:grid-cols-3 lg:grid-cols-10 md:grid-cols-5">
                     @foreach ($categories as $category)
                         <div class="flex items-center mb-2">
                             <input id="category-{{ $category['id'] }}" type="checkbox" name="event_category_ids[]"
