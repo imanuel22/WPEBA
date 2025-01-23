2ad830d3426299d55fbab5167d785d5b7b93090b
Demonic <134345277+IKomangYoga@users.noreply.github.com>
2025-01-24 00:14:20 +0800

    admin

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -34,10 +34,10 @@
                         id="dropdown-user">
                         <div class="px-4 py-3" role="none">
                             <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                Neil Sims
+                                {{ session('name') }}
                             </p>
                             <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                neil.sims@flowbite.com
+                                {{ session('email') }}
                             </p>
                         </div>
                         <ul class="py-1" role="none">
