e98d4df943fca41165d0fbefa1b1c49dbb761f1a
Demonic <134345277+IKomangYoga@users.noreply.github.com>
2025-01-23 21:52:01 +0800

    update admin
    update alert dll

--- resources/views/admin/dokumentasi.blade.php
+++ resources/views/admin/dokumentasi.blade.php
@@ -57,8 +57,12 @@
             <tbody>
                 @foreach ($documentation as $row)
                     <tr class="cursor-pointer bg-white">
-                        <td><img src="{{ env('APP_API_IMG_URL') }}/documentations/{{ $row['image'] }}"
-                                alt="{{ $row['event']['title'] }}"></td>
+
+                        <td class="max-w-sm">
+                            <img class="rounded-lg w-52"
+                                src="{{ config('services.api.img') }}/documentations/{{ $row['image'] }}"
+                                alt="{{ $row['image'] }}">
+                        </td>
                         <td>{{ $row['description'] }}</td>
                         <th>{{ $row['event']['title'] }}</th>
                         <td>
@@ -167,6 +171,7 @@
                             <input type="name" name="name" id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 placeholder="nama" required />
+                            
                         </div>
                         <div>
                             <label for="email"
--- resources/views/admin/user/account.blade.php
+++ resources/views/admin/user/account.blade.php
@@ -7,6 +7,7 @@
     <div class="mb-5 border-b-4 border-slate-700 ">
         <h3 class="text-3xl">DATA AKUN</h3>
     </div>
+
     @if (session()->has('message'))
         <div id="alert-3"
             class="flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
@@ -151,7 +152,7 @@
                     <tr class="cursor-pointer bg-white">
                         <td class="font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $row['name'] }}</td>
                         <td>{{ $row['email'] }}</td>
-                        <td><img class="w-40" src="{{ env('APP_API_IMG_URL') }}/user/{{ $row['profile'] }}"
+                        <td><img class="w-56 rounded-lg" src="{{ env('APP_API_IMG_URL') }}/user/{{ $row['profile'] }}"
                                 alt="{{ $row['name'] }}"></td>
                         <td>
                             <button type="button" data-modal-target="edit-{{ $row['id'] }}"
@@ -281,7 +282,10 @@
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
                             <input type="text" name="name" id="name"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                placeholder="nama" required />
+                                placeholder="nama" />
+                                @error('name')
+                                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                                @enderror
                         </div>
                         <div>
                             <label for="email"
@@ -289,6 +293,9 @@
                             <input type="email" name="email" id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 placeholder="email@gmail.com" required />
+                                @error('email')
+                                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                                @enderror
                         </div>
                         <div>
                             <label for="password"
@@ -296,6 +303,9 @@
                             <input type="password" name="password" id="password"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 placeholder="password" required />
+                                @error('password')
+                                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                                @enderror
                         </div>
                         <div>
                             <label for="profile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -303,6 +313,9 @@
                             <input
                                 class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                                 id="file_input" type="file" name="profile">
+                                @error('profile')
+                                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                                @enderror
                         </div>
                         <button type="submit"
                             class="text-white inline-flex w-full justify-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
